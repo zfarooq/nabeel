9d09486742bd98563349055308b6d4e0a5d8fcec
Zahid Farooq <zahid.farooq@speridian.com>
2020-04-06 19:38:40 +0500

    RoleID

--- NB.ClientPortal/NB.ClientPortal/Client.WebAPI/Entities/User.cs
+++ NB.ClientPortal/NB.ClientPortal/Client.WebAPI/Entities/User.cs
@@ -7,9 +7,15 @@ namespace Client.WebAPI.Entities
 {
 	public class User:TrackableEntityBase<int>
 	{	
+		public User()
+		{
+			Role = new Role();
+		}
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Username { get; set; }
 		public string Password { get; set; }
+		public Nullable<int> RoleId { get; set; }
+		public virtual Role Role { get; set; }
 	}
 }
